bd7841a18755612bf7b2411f01a0baccd6714918
Marian Halachev <mariqn03032@gmail.com>
2020-03-19 10:11:26 +0200

    2

---
+++ Home-Projects/Miniorska_Zadacha_27_Word_Zad3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miniorska_Zadacha_27_Word_Zad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> minior = new Dictionary<string, int>();
+            while (true)
+            {
+                string material = Console.ReadLine();
+                if (material == "stop")
+                {
+                    break;
+
+                }
+                int price = int.Parse(Console.ReadLine());
+
+
+                if (minior.ContainsKey(material))
+                {
+                    minior[material] += price;
+                }
+                else
+                {
+                    minior.Add(material, price);
+                }
+
+            }
+
+            foreach (var item in minior)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
